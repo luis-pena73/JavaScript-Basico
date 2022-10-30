a218e0a4477c251b6ee4ac504a64a7f52aa69adc
luis fernando peña <luis.fernandop@peaku.ai>
2022-10-30 11:39:45 -0500

    nuevos cambios en el ejercicio 4

---
+++ sesion-04-strings/ejercicio4.js
@@ -0,0 +1,18 @@
+let nombre = "luis fernando";
+console.log(nombre);
+
+let apellido = " peña";
+console.log(apellido);
+
+let estudiante = `${nombre}${apellido}`;
+console.log(estudiante)
+
+let estudianteMayus = (estudiante.toUpperCase());
+console.log (estudianteMayus);
+
+let estudianteMinus = (estudianteMayus.toLowerCase());
+console.log (estudianteMinus);
+console.log (estudiante.length);
+console.log (nombre.charAt(0))
+console.log (apellido.charAt(3))
+console.log (estudiante.replaceAll(" ",""));
