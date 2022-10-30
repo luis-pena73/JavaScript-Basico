535f5754c7c0af9488306209a6c5948a76c7f0fe
luis fernando peña <luis.fernandop@peaku.ai>
2022-10-30 08:10:28 -0500

    nuevos cambios

---
+++ sesion-03-estructuras-de-control/factorial-break.js
@@ -0,0 +1,10 @@
+let lista = [10, 11, 12, 13, 15, 16, 17];
+
+lista.forEach(lista => {
+    })
+if (lista >= 12){
+    console.log ("este es un numero par");
+    }else {
+        console.log ("este esyo es una lista")
+    }
+console.log (lista)
