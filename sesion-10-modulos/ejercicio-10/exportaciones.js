c5161ce44b5929032051b665ba83a6123af2bca1
luis fernando peña <luis.fernandop@peaku.ai>
2022-11-28 14:21:43 -0500

    nuevos cambios en la sesion 10

--- sesion-10-modulos/ejercicio-10/exportaciones.js
+++
@@ -1,6 +0,0 @@
-import {suma,eleva} from './modulos/matematicas.js'
-
-const sum = suma(4, 5)
-const potencia = eleva (9, 7)
-console.log(sum)
-console.log(potencia)
--- sesion-10-modulos/ejercicio/index.js
+++ sesion-10-modulos/ejercicio/index.js
@@ -1,8 +1,10 @@
 import { suma, multiplica } from "./controller.js";
+import chalk from 'chalk';
+const log = console.log;
 
 const sum1 = suma (1, 2)
 
 const sum2 = suma (4, 5)
 
 const mul = multiplica (sum1, sum2)
-console.log(mul)
+console.log(chalk.green(mul))   
